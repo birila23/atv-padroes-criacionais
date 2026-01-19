74714fad53e49bb90e4bc90f25027640a39c2ea8
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2026-01-18 22:04:40 -0300

    refatorando a parte de configuracaoJogo com singleton

--- src/main/java/br/edu/ifpb/ads/padroes/atv1/rpg/config/ConfiguracaoJogo.java
+++ src/main/java/br/edu/ifpb/ads/padroes/atv1/rpg/config/ConfiguracaoJogo.java
@@ -2,12 +2,19 @@ package br.edu.ifpb.ads.padroes.atv1.rpg.config;
 
 public class ConfiguracaoJogo {
 
+    private static ConfiguracaoJogo instance;
     private int nivelDificuldade;
 
     public ConfiguracaoJogo() {
         this.nivelDificuldade = 1;
     }
 
+    public static ConfiguracaoJogo getInstance() {
+        if (instance == null) {
+            instance = new ConfiguracaoJogo();
+        }
+        return instance;
+    }
     public int getNivelDificuldade() {
         return nivelDificuldade;
     }
