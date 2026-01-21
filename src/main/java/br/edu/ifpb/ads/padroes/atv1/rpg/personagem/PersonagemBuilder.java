3ec9e8b566147ac7822cc4602d6dba1262027c9f
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2026-01-20 22:29:20 -0300

    criando uma classe para facilitar a criação do construtor do personagem

--- src/main/java/br/edu/ifpb/ads/padroes/atv1/rpg/personagem/PersonagemBuilder.java
+++ src/main/java/br/edu/ifpb/ads/padroes/atv1/rpg/personagem/PersonagemBuilder.java
@@ -1,4 +1,54 @@
 package br.edu.ifpb.ads.padroes.atv1.rpg.personagem;
 
+import br.edu.ifpb.ads.padroes.atv1.rpg.Arma;
+import br.edu.ifpb.ads.padroes.atv1.rpg.Armadura;
+import br.edu.ifpb.ads.padroes.atv1.rpg.Personagem;
+
 public class PersonagemBuilder {
+    private String nome;
+    private String raca;
+    private String classe;
+    private int forca;
+    private int inteligencia;
+    private int agilidade;
+    private int vida;
+    private int mana;
+    private Arma arma;
+    private Armadura armadura;
+    private String[] habilidades;
+
+    public PersonagemBuilder nome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+    public PersonagemBuilder raca(String raca) {
+        this.raca = raca;
+        return this;
+    }
+    public PersonagemBuilder classe(String classe) {
+        this.classe = classe;
+        return this;
+    }
+    public PersonagemBuilder atributos(int forca, int inteligencia, int agilidade,  int vida,  int mana) {
+        this.forca = forca;
+        this.inteligencia = inteligencia;
+        this.agilidade = agilidade;
+        this.vida = vida;
+        this.mana = mana;
+        return this;
+    }
+
+    public PersonagemBuilder equipamento(Arma arma, Armadura armadura) {
+        this.arma = arma;
+        this.armadura = armadura;
+        return this;
+    }
+
+    public PersonagemBuilder habilidades(String[] habilidades) {
+        this.habilidades = habilidades;
+        return this;
+    }
+    public Personagem build() {
+        return new Personagem(nome, raca, classe, forca, inteligencia, agilidade, vida, mana, arma, armadura, habilidades);
+    }
 }
